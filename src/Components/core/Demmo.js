927b79a71b3225e0273b93ceba7ebbeb08e113b0
FotaKelly <fotsamanda@gmail.com>
2026-02-23 10:43:24 +0100

    mis a jour

--- src/Components/core/Demmo.js
+++ src/Components/core/Demmo.js
@@ -23,20 +23,21 @@ const Demmo = (props) => {
  
 return(
    <Flex>
-                  <Box maxW="sm" borderWidth="1px" shadow="custom" borderRadius="lg" overflow="hidden">
-                  <Image src={props.image} alt={props.imageAlt} p="4" width="100%"   />
-          
-                      <Box p="4" spaceY="2">
-                          
-                              <Text fontWeight="medium" color="fg">
-                              {props.title}
-                              </Text>
-                              <HStack color="fg.muted">
-                             
-                          </HStack>
-                      </Box>
-                  </Box>
-              </Flex>
+        <Box maxW="sm" borderWidth="1px" shadow="custom" borderRadius="lg" overflow="hidden">
+        <Image src={props.image} alt={props.imageAlt} p="4" width="100%"   />
+
+            <Box p="4" spaceY="2">
+                
+            <Text fontWeight="medium" color="fg">
+            {props.title}
+            </Text>
+
+            <HStack color="fg.muted">
+                hhhh
+            </HStack>
+            </Box>
+        </Box>
+    </Flex>
 )
 
 }
